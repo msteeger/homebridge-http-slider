8ce397444bfbbf1eb99b3ce5fea3b354bff0f5cf
datMaffin <maffinmaffinmaffinmaffin@gmail.com>
2018-09-01 12:08:45 +0200

    Add lightbulb slider

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/*
+ *  _   _ _   _              ____  _ _     _
+ * | | | | |_| |_ _ __      / ___|| (_) __| | ___ _ __
+ * | |_| | __| __| '_ \ ____\___ \| | |/ _` |/ _ \ '__|
+ * |  _  | |_| |_| |_) |_____|__) | | | (_| |  __/ |
+ * |_| |_|\__|\__| .__/     |____/|_|_|\__,_|\___|_|
+ *               |_|
+ *
+ * A homebridge plugin
+ *
+ */
+
 var request = require('request');
 var Service, Characteristic;
 
@@ -10,13 +22,69 @@ module.exports = function(homebridge) {
 
 function SliderAccessory(log, config) {
     this.log = log;
-    log('loaded');
+    log('Starting http-slider');
+    this.config = config;
 }
 
 SliderAccessory.prototype = {
 
     getServices: function() {
-        var service = [];
-        return service;
+        var services = [];
+
+        if (this.config.service === 'LightBulb') {
+            this.lightBulb = new Service.Lightbulb('LightBulb Slider');
+            this.lightBulb
+                .getCharacteristic(Characteristic.On)
+                .on('get', function(callback) {
+                    callback(null, true);
+                })
+                .on('set', function(state, callback) {
+                    callback(null);
+                    reset(this.lightBulb, Characteristic.On);
+                }.bind(this));
+            this.lightBulb
+                .getCharacteristic(Characteristic.Brightness)
+                .on('set', function(state, callback) {
+                    var number_of_states = this.config.http_states.length;
+                    var interval_half = 100 / number_of_states;
+                    var interval = 2 * interval_half;
+                    for (var i = 0; i < number_of_states; i++) {
+                        var section = interval * i;
+                        if (state - section < interval_half && 
+                            state - section >= -interval_half) {
+                            request(this.config.http_states[i], function(){});
+                            callback(null, section);
+                            return;
+                        }
+                    }
+                }.bind(this));
+
+            services.push(this.lightBulb);
+        }
+
+        return services;
     }
+};
+
+/**
+ * Resets a characteristic of a service by using a getter callback with a 
+ * specific delay
+ *
+ * @param {Service} service The service
+ * @param {Characteristic} characterisitc The characteristic of the service to reset
+ * @param {number} delay The delay when the reset takes place
+ */
+var reset = function(service, characteristic, delay){
+    if (!delay) {
+        // default value without ECMAscript 6 features
+        delay = 1000;
+    }
+
+    setTimeout(function() {
+        service.getCharacteristic(characteristic).emit('get', function(error, newValue) {
+            service
+                .getCharacteristic(characteristic)
+                .updateValue(newValue);
+        });
+    }, delay);
 };
--- test/test.js
+++ test/test.js
@@ -6,7 +6,14 @@ var nock = require('nock');
 /*
  * Mocks
  */
+
 var log = function(){};
+
+/*
+var log = console.log;
+log.debug = console.log;
+*/
+
 var Homebridge = require('./mocks/homebridge.js')(undefined);
 var Service = Homebridge.hap.Service;
 var Characteristic = Homebridge.hap.Characteristic;
@@ -34,14 +41,72 @@ describe('Reqister Accessory', function() {
 });
 
 describe('Accessory configs', function() {
-    var config = {};
+    var config;
     var slider;
-    describe('thermostat 1', function() {
-        config = {};
-        slider = new AccessoryType(log, config);
-    });
     describe('light 1', function() {
+        config = {
+            accessory: 'Slider',
+            service:   'LightBulb',
+            http_states:    ['http://127.0.0.1/0', 'http://127.0.0.1/1']
+        };
+        slider = new AccessoryType(log, config);
+        var light_service;
+        it('should include the light bulb service', function() {
+            slider.getServices().forEach(function(s) {
+                if (s instanceof Service.Lightbulb)
+                    light_service = s;
+            });
+            assert.ok(light_service);
+        });
+        it('should make the correct request 1', function(done) {
+            nock('http://127.0.0.1').get('/0').reply(200, '');
+            light_service
+                .getCharacteristic(Characteristic.Brightness)
+                .emit('set', 40, function(){});
+            setTimeout(function() {
+                if (nock.isDone())
+                    done();
+                else
+                    done('nock was not done!');
+            }, 1500);
+        });
+        it('should make the correct request 2', function(done) {
+            nock('http://127.0.0.1').get('/1').reply(200, '');
+            light_service
+                .getCharacteristic(Characteristic.Brightness)
+                .emit('set', 60, function(){});
+            setTimeout(function() {
+                if (nock.isDone())
+                    done();
+                else
+                    done('nock was not done!');
+            }, 1500);
+        });
+        it('should return the correct value 1', function(done) {
+            nock('http://127.0.0.1').get('/0').reply(200, '');
+            light_service
+                .getCharacteristic(Characteristic.Brightness)
+                .emit('set', 40, function(error, state){
+                    if (state === 0)
+                        done();
+                    else
+                        done('wrong state returned: ' + state);
+                });
+        });
+        it('should return the correct value 2', function(done) {
+            nock('http://127.0.0.1').get('/1').reply(200, '');
+            light_service
+                .getCharacteristic(Characteristic.Brightness)
+                .emit('set', 60, function(error, state){
+                    if (state === 100)
+                        done();
+                    else
+                        done('wrong state returned: ' + state);
+                });
+        });
     });
     describe('fan 1', function() {
+    });
+    describe('thermostat 1', function() {
     });
 });
